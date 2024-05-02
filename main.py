194a6cb0aea1229c6c1ebabd60025b1ee708e81e
Sara-Nasher <saranasher83@gmail.com>
2024-05-02 16:23:43 +0330

    update main

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from rich.console import Console
+from rich.console import Console, OverflowMethod
 from rich.table import Table
+import json
+from rich import print
+from typing import List
+import os
+import datetime
 
-table = Table( width=40, show_header=True, show_lines=True, style="bold magenta", title_justify='center', caption_justify='center')
 
-table.add_column("Do you have an account?", justify="center", style="italic", no_wrap=True)
-table.add_row("1. sign up", style='bold red')
-table.add_row("2. login", style="bold green")
-table.add_row("3. exit", style='bold yellow')
-console = Console()
-console.print(table)
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def main():
+    clear_screen()
+    while True:
+        table = Table( width=40, show_header=True, show_lines=True, style="bold magenta")
+
+        table.add_column("Do you have an account?", justify="center", style="italic", no_wrap=True)
+        table.add_row("1. sign up", style='bold red')
+        table.add_row("2. login", style="bold green")
+        table.add_row("3. exit", style='bold yellow')
+        console = Console()
+        console.print(table)
+        choice = input("Choose an option: ")
+        clear_screen()
+
+
+
+if __name__ == "__main__":
+    main()
