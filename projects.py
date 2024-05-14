fda52e8e9e991c8f6249c942f332cae1f30a97e8
Setayesh yarijani <152412073+setayeshyarijani@users.noreply.github.com>
2024-05-14 23:51:35 +0330

    debug create project

--- projects.py
+++ projects.py
@@ -96,67 +96,6 @@ class ProjectManager:
         self.save_projects(projects)
         return Project(project_id, title, owner)
 
-    def add_member_to_project(self, project_id, member, username):
-        projects = self.load_projects()
-        users = UserManager().load_users()
-        
-        if project_id in projects:
-            project = projects[project_id]
-            if project["owner"] == username:
-                if member in users:
-                    project["members"] = project.get("members", [])
-                    if member not in project["members"]:
-                        project["members"].append(member)
-                        print(f"Member '{member}' added to project '{project['title']}' successfully.")
-                        input("Press Enter to continue...")
-                        clear_screen()
-                        
-                    else:
-                        print(f"Member '{member}' is already a member of project '{project['title']}'.")
-                        input("Press Enter to continue...")
-                        clear_screen()
-                    self.save_projects(projects)
-                else:
-                    print("[bold red]User not found![/bold red]")
-                    input("Press Enter to continue...")
-                    clear_screen()
-            else:
-                print("[bold red]You are not the owner of this project![/bold red]")
-                input("Press Enter to continue...")
-                clear_screen()
-        else:
-            print(f"Project with ID '{project_id}' not found.")
-            input("Press Enter to continue...")
-            clear_screen()
-
-    def remove_member_from_project(self, project_id, member, username):
-        projects = self.load_projects()
-        if project_id in projects:
-            project = projects[project_id]
-            if project["owner"] == username:
-                if "members" in project and member in project["members"]:
-                    project["members"].remove(member)
-                    print(f"Member '{member}' removed from project '{project['title']}' successfully.")
-                    input("Press Enter to continue...")
-                    clear_screen()
-                    self.save_projects(projects)
-                    return True
-                else:
-                    print(f"Member '{member}' is not a member of project '{project['title']}'")
-                    input("Press Enter to continue...")
-                    clear_screen()
-                    return False
-            else:
-                print("[bold red]You are not the owner of this project![/bold red]")
-                input("Press Enter to continue...")
-                clear_screen()
-                return False
-        else:
-            print(f"Project with ID '{project_id}' not found.")
-            input("Press Enter to continue...")
-            clear_screen()
-            return False
-
 
         
 def clear_screen():
@@ -395,29 +334,6 @@ def show_project(username):
 
     selected_project = person_projects[project_choice]
     return selected_project
-            
-def edit_project_menu(username, selected_project):
-    project_manager = ProjectManager()
-    while True:
-        clear_screen()
-        print("1. Add Member to Project")
-        print("2. Remove Member from Project")
-        print("3. Manage Tasks")
-        print("4. Exit")
-        choice = input("Choose an option: ")
-        if choice == '1':
-            member = input("Enter the username of the member you want to add: ")
-            project_manager.add_member_to_project(selected_project['project_id'], member, username)
-        elif choice == '2':
-            member = input("Enter the username of the member you want to remove: ")
-            project_manager.remove_member_from_project(selected_project['project_id'], member, username)
-        elif choice == '3':
-            print("Manage Tasks option selected.")
-        elif choice == '4':
-            break
-        else:
-            print("Invalid choice. Please try again.")
-            input("Press Enter to continue...")
 
 def account(username):
     project_manager = ProjectManager()
@@ -435,10 +351,6 @@ def account(username):
             title = input("Enter project title: ")
             project_manager.create_project(project_id, title, username)
             
-        elif choice == '2':
-            selected_project = show_project(username)
-            if selected_project:
-                edit_project_menu(username, selected_project)
 
         elif choice == '5':
             break
