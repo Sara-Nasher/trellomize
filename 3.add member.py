c0c554127f4fa1a9a801fffa2a0c61fdd34447a4
Setayesh yarijani <152412073+setayeshyarijani@users.noreply.github.com>
2024-05-25 02:10:06 +0330

    add member unittest

--- 3.add member.py
+++ 3.add member.py
@@ -9,8 +9,6 @@ class TestProjectManager(unittest.TestCase):
         self.user_manager = UserManager()
         self.projects_file = "Account/projects.json"
         self.users_file = "Account/account.json"
-
-        # Load the projects and users from the files
         with open(self.projects_file, "r") as file:
             self.projects = json.load(file)
         with open(self.users_file, "r") as file:
@@ -20,60 +18,36 @@ class TestProjectManager(unittest.TestCase):
         project_id = "ID"
         member = "q"
         username = "Sara"
-
-    # Clear the project's member list before adding the member
         self.projects[project_id]["members"] = []
-
-    # Check that the member is not already in the project
         self.assertNotIn(member, self.projects[project_id].get("members", []))
-
-    # Add the member to the project
         self.project_manager.add_member_to_project(project_id, member, username)
-
-    
 
     def test_add_member_to_project_already_member(self):
         project_id = "ID"
         member = "q"
         username = "Sara"
-
-    # Add the member to the project first
         self.project_manager.add_member_to_project(project_id, member, username)
-
-    # Try to add the member again
         self.assertFalse(self.project_manager.add_member_to_project(project_id, member, username))
 
     def test_add_member_to_project_user_not_found(self):
         project_id = "ID"
         member = "non_existent_user"
         username = "Sara"
-
-        # Try to add the member to the project
         self.project_manager.add_member_to_project(project_id, member, username)
-
-        # Check that an error message is printed
         self.assertEqual(self.project_manager.add_member_to_project(project_id, member, username), None)
 
     def test_add_member_to_project_not_owner(self):
         project_id = "ID"
         member = "q"
         username = "not_owner"
-
-        # Try to add the member to the project
         self.project_manager.add_member_to_project(project_id, member, username)
-
-        # Check that an error message is printed
         self.assertEqual(self.project_manager.add_member_to_project(project_id, member, username), None)
 
     def test_add_member_to_project_project_not_found(self):
         project_id = "non_existent_project"
         member = "new_member"
         username = "Sara"
-
-        # Try to add the member to the project
         self.project_manager.add_member_to_project(project_id, member, username)
-
-        # Check that an error message is printed
         self.assertEqual(self.project_manager.add_member_to_project(project_id, member, username), None)
 
 if __name__ == "__main__":
